96b7af594c418e7855beed6975be7b596a9f7858
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2025-07-17 13:46:10 -0300

    binding data directly to html

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ new Vue({
     data: {
         name: 'Leandro',
         job: 'Web Developer',
+        website: 'https://google.com',
+        websiteTag: '<a href="https://google.com">Google</a>'
     },
     methods: {
         greet: function(time) {
