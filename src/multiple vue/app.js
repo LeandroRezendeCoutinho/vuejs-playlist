be77c134f9d2000882c8e7f10dee2035a3b0f10d
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2025-07-18 16:40:51 -0300

    using multiple vue instances

---
+++ src/multiple vue/app.js
@@ -0,0 +1,29 @@
+var one = new Vue({
+    el: '#vue-app-one',
+    data: {
+        title: 'Vue app one'
+    },
+    methods: {
+
+    },
+    computed: {
+        greet: function () {
+            return 'Hello ' + this.title;
+        }
+    }
+});
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue app two'
+    },
+    methods: {
+
+    },
+    computed: {
+        greet: function () {
+            return 'Hello ' + this.title;
+        }
+    }
+});
